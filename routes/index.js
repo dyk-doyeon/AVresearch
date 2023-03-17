e79b5868d49db135f0a0762612174c758f215ad8
Doyeon Kim <90243955+Doyeon-k-fanshawe@users.noreply.github.com>
2023-03-17 14:15:29 -0400

    ADD: SET DEFAULT VALUES FOR CSS

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.render('index', { title: 'PLAY FAIR' });
+  res.render('index', { title: 'A/V Research' });
 });
 
 router.get('/audio', function (req, res, next) {
